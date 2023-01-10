f561c435c41f6f4ff0ada61e1eb99db93af074fb
unknown <erdematik8@gmail.com>
2023-01-10 21:28:33 +0300

    removed utm lib

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Converting Latitude-Longitude To Decimal Coordinate
 I searched that, but I can not find simple and modern Vanilla JS coordinate converter. Then I coded this :)
 Input value should be ##°##'##.##" N(or S) ##°##'##.##" E(or W) (etc: 41° 1'0.18"N  28°59'6.55"E)
+Edit: improved to also accept as ##°##'##,##"(so it can include comma as decimal separator)
 Best!
---
+++ app.js
@@ -0,0 +1,39 @@
+"use strict";
+
+const inpValue = document.getElementById('inputCoord')
+const inpbtn = document.getElementById('inputBtn')
+
+let coordValue;
+
+const inpCoord = inpbtn.addEventListener('click', function(event){
+    event.preventDefault();
+    coordValue = inpValue.value
+    console.log(parseLatLong(coordValue));
+})
+
+
+function parseLatLong(input) {
+
+if(!( input.toUpperCase() != input.toLowerCase())) {   // if geodirection abbr. isn't exist, it should be already decimal notation
+    return `${input}:the coordinate already seems as decimal or not valid format`
+}
+const parts = input.split(/[°'"]+/).join(' ').split(/[^\w\S]+/); // thanks to Shannon Antonio Black(StackoverFlow) for regEx patterns 
+const replacedComa = parts.map(el=>el.replace(',','.')) // in case the given numbers include comma as decimal separator
+const geoLetters = replacedComa.filter(el=> !(+el))
+geoLetters.filter(el=>{return el !== ''} );
+const coordNumber = replacedComa.filter(n=>(+n)).map(nr=>+nr)
+const latNumber = coordNumber.slice(0,(coordNumber.length/2))
+const longNumber = coordNumber.slice((coordNumber.length/2))
+const reducer = function(acc,coord,curInd){
+   return (acc + (coord/Math.pow( 60, curInd++ )))
+}
+let latDec = latNumber.reduce(reducer)
+let longDec = longNumber.reduce(reducer)
+
+if((geoLetters[0].toUpperCase()==='S')||(geoLetters[0].toUpperCase()==='G')) latDec = -latDec // G is abr. of güney(it means South in Turkish)
+if((geoLetters[1].toUpperCase()==='W')||(geoLetters[1].toUpperCase()==='B')) longDec= -longDec // B is abr. of batı (it means west in Turkish)
+
+
+return [ latDec,geoLetters[0],longDec,geoLetters[1]]
+}
+
